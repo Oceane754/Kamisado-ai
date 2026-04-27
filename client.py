649aa139b037ca0f0edd84c5c5b2a358b8878d13
Oceane Lumpungu <23202@ecam.be>
2026-04-27 16:43:46 +0200

    Response to ping

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import struct
 SERVER_IP = "127.0.0.1"  # L'adresse IP du serveur (localhost = mon propre ordinateur)
 SERVER_PORT = 3000 # Le port où le serveur écoute
 
+CLIENT_IP = "127.0.0.1" # L'adresse IP où l'IA écoute
 CLIENT_PORT = 8888 # Mon port
 
 def envoyer_message(sock, message_dict):
@@ -43,6 +44,37 @@ def inscription():
         reponse = recevoir_message(client_socket)
         print(f"Réponse du serveur : {reponse}")
 
+def lancer_ecoute():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ecoute_socket:
+        # Permet de relancer le programme tout de suite
+        ecoute_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        ecoute_socket.bind((CLIENT_IP, CLIENT_PORT))
+        ecoute_socket.listen()
+        print(f"Notre IA est sur écoute sur le port {CLIENT_PORT}...")
+        
+        # Boucle infinie pour écouter en permanence
+        while True:
+            conn, adresse = ecoute_socket.accept()
+            
+            with conn:
+                # Usage de la connexion spécifique entre le serveur et notre IA
+                requete = recevoir_message(conn)
+                
+                if requete:
+                    print(f"--> Message reçu du serveur : {requete}")
+                    
+                    type_requete = requete.get("request")
+                    
+                    if type_requete == "ping":
+                        reponse = {"response": "pong"}
+                        envoyer_message(conn, reponse)
+                        print("<-- Réponse 'pong' envoyée !\n")
+                        
+                    elif type_requete == "play":
+                        print("Le serveur me demande de jouer !")
+                        pass
+
 # Lancement du code
 if __name__ == "__main__":
     inscription()
+    lancer_ecoute()
