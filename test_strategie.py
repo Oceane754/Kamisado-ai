967e0d0540588e4e585b905d4d252bd8076f8594
Belkassmi <23276@ecam.be>
2026-04-30 16:36:17 +0200

    Add tests for IA:  color constraint

--- test_strategie.py
+++ test_strategie.py
@@ -19,7 +19,7 @@ def test_bloque():
     assert move[0] == move[1]
 
 
-#test2 :couleur imposee
+#test2 :couleur imposee ; est ce que l'ia choisi bien la couleur imposée pour la tour 
 
 def test_couleur_imposee():
     board = [[("white", None) for _ in range(8)] for _ in range(8)]
@@ -36,26 +36,3 @@ def test_couleur_imposee():
     move = choose_move(state)
 
     assert move[0] == [6, 4]
-
-#test3 : test danger
-
-def test_evite_danger():
-    board = [[("white", None) for _ in range(8)] for _ in range(8)]
-
-    #ma Tour
-    board[6][3] = ("blue", ("blue", "dark"))
-
-    #Tour ADVERSE prête à gagner
-    board[1][3] = ("blue", ("blue", "light"))
-
-    state = {
-        "board": board,
-        "current": 0,
-        "color": None
-    }
-
-    move = choose_move(state)
-
-    #si tu avances tout droit; tu donnes bleu; il gagne = big danger 
-    #donc l' IA NE DOIT PAS faire ça
-    assert move[1] != [0, 3]
