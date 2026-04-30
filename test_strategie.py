a7fff5572b9f5e600853a713e608a8a52b9ae7fa
Belkassmi <23276@ecam.be>
2026-04-30 16:22:31 +0200

    Add tests: avoid danger

--- test_strategie.py
+++ test_strategie.py
@@ -1,26 +1,11 @@
 from strategie import choose_move
 
-def test_simple():
-    board = [[("white", None) for _ in range(8)] for _ in range(8)]
-    board[6][3] = ("blue", ("blue", "dark"))
+#test1 ; si ia bloquée 
 
-    state = {
-        "board": board,
-        "current": 0,
-        "color": None
-    }
-
-    move = choose_move(state)
-
-    assert move is not None
-
-    pass
-
-#test 2 ; si ia bloquée 
 def test_bloque():
     board = [[("white", None) for _ in range(8)] for _ in range(8)]
 
-    # tour en haut doncaucun move
+    # tour en haut donc aucun move
     board[0][0] = ("blue", ("blue", "dark"))
 
     state = {
@@ -32,3 +17,45 @@ def test_bloque():
     move = choose_move(state)
 
     assert move[0] == move[1]
+
+
+#test2 :couleur imposee
+
+def test_couleur_imposee():
+    board = [[("white", None) for _ in range(8)] for _ in range(8)]
+
+    board[6][3] = ("blue", ("blue", "dark"))
+    board[6][4] = ("red", ("red", "dark"))
+
+    state = {
+        "board": board,
+        "current": 0,
+        "color": "red"
+    }
+
+    move = choose_move(state)
+
+    assert move[0] == [6, 4]
+
+#test3 : test danger
+
+def test_evite_danger():
+    board = [[("white", None) for _ in range(8)] for _ in range(8)]
+
+    #ma Tour
+    board[6][3] = ("blue", ("blue", "dark"))
+
+    #Tour ADVERSE prête à gagner
+    board[1][3] = ("blue", ("blue", "light"))
+
+    state = {
+        "board": board,
+        "current": 0,
+        "color": None
+    }
+
+    move = choose_move(state)
+
+    #si tu avances tout droit; tu donnes bleu; il gagne = big danger 
+    #donc l' IA NE DOIT PAS faire ça
+    assert move[1] != [0, 3]
